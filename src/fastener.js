31a731bd978498148909f22bcc2635f4f2584dd7
Vesa Karvonen <vesa.a.j.k@gmail.com>
2016-04-22 15:14:35 +0200

    Simplified

--- src/fastener.js
+++ src/fastener.js
@@ -9,36 +9,31 @@ export const get = z => z.focus
 export const set = R.curry((focus, z) => ({...z, focus}))
 export const modify = R.curry((f, z) => set(f(get(z)), z))
 
-export const up = z => z.up(z)
+export const up = ({left, focus, right, keys, up}) => {
+  if (keys) {
+    return {focus: R.zipObj(keys, [...left, focus, ...right]), ...up}
+  } else if (up) {
+    return {focus: [...left, focus, ...right], ...up}
+  } else {
+    return undefined
+  }
+}
 
-export const downTo = R.curry((k, {left: upLeft, focus, right: upRight, up}) => {
+export const downTo = R.curry((k, {focus, ...up}) => {
   if (isObject(focus)) {
     const keys = R.keys(focus)
     const values = R.values(focus)
     const i = keys.findIndex(R.equals(k))
-    return {
-      left: values.slice(0, i),
-      focus: values[i],
-      right: values.slice(i+1),
-      up: ({left, focus, right}) => ({
-        left: upLeft,
-        focus: R.zipObj(keys, [...left, focus, ...right]),
-        right: upRight,
-        up
-      })
-    }
+    return {left: values.slice(0, i),
+            focus: values[i],
+            right: values.slice(i+1),
+            keys,
+            up}
   } else if (isArray(focus)) {
-    return {
-      left: focus.slice(0, k),
-      focus: focus[k],
-      right: focus.slice(k+1),
-      up: ({left, focus, right}) => ({
-        left: upLeft,
-        focus: [...left, focus, ...right],
-        right: upRight,
-        up
-      })
-    }
+    return {left: focus.slice(0, k),
+            focus: focus[k],
+            right: focus.slice(k+1),
+            up}
   } else {
     return undefined
   }
@@ -60,22 +55,21 @@ export const downHead = downMost(true)
 export const downLast = downMost(false)
 
 // FYI: The left and right ops are not accidentally O(n).  I'm just lazy. :)
-export const left = ({left, focus, right, up}) =>
+export const left = ({left, focus, right, ...rest}) =>
   left.length === 0 ? undefined : {
     left: R.dropLast(1, left),
     focus: R.last(left),
     right: R.prepend(focus, right),
-    up}
+    ...rest}
 
-export const right = ({left, focus, right, up}) =>
+export const right = ({left, focus, right, ...rest}) =>
   right.length === 0 ? undefined : {
     left: R.append(focus, left),
     focus: R.head(right),
     right: R.drop(1, right),
-    up}
+    ...rest}
 
-export const toZipper = focus =>
-  ({left: [], focus, right: [], up: () => undefined})
+export const toZipper = focus => ({left: [], right: [], focus})
 
 export const fromZipper = z =>
   pass(up(z), zz => zz ? fromZipper(zz) : get(z))
