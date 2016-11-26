0903cae4cc6a35b732f08c2a575c030883811c83
Vesa Karvonen <vesa.a.j.k@gmail.com>
2016-11-26 17:34:55 +0200

    Factor object ctor out

--- bench/bench.js
+++ bench/bench.js
@@ -4,8 +4,10 @@ const F = require("../lib/fastener")
 
 const inc = x => typeof x === "number" ? x + 1 : x
 
+const nested = [{x:1,y:[2,{d:3},4],z:{a:5}}]
+
 const bs = [
-  'F.fromZipper(F.everywhere(inc, F.toZipper([{x:1,y:[2,{d:3},4],z:{a:5}}])))',
+  'F.fromZipper(F.everywhere(inc, F.toZipper(nested)))',
 ]
 
 const s = new require("benchmark").Suite()
