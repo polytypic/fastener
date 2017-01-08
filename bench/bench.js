6137125b81d65f4dff544610e3597a480ad81719
Vesa Karvonen <vesa.a.j.k@gmail.com>
2017-01-08 21:29:40 +0200

    Run quick bench on travis.  Updated deps.

--- bench/bench.js
+++ bench/bench.js
@@ -1,6 +1,9 @@
 'use strict';
 
+const I = require("infestines")
 const F = require("../lib/fastener")
+const R = require("ramda")
+const sprintf = require("sprintf-js").sprintf
 
 const inc = x => typeof x === "number" ? x + 1 : x
 
@@ -9,13 +12,43 @@ const vs10 = Array(10).fill(1)
 const vs100 = Array(100).fill(1)
 const vs1000 = Array(1000).fill(1)
 
-const bs = [
-  'F.fromZipper(F.everywhere(inc, F.toZipper(nested)))',
-  'F.fromZipper(F.everywhere(inc, F.toZipper(vs10)))',
-  'F.fromZipper(F.everywhere(inc, F.toZipper(vs100)))',
-  'F.fromZipper(F.everywhere(inc, F.toZipper(vs1000)))'
-]
+const Benchmark = require("benchmark")
+Benchmark.options.maxTime = Number(process.argv[2]) || 10
 
-const s = new require("benchmark").Suite()
-bs.forEach(b => s.add(b, eval("() => " + b)))
-s.on('cycle', e => console.log(String(e.target))).run()
+R.forEach(bs => {
+  global.gc()
+  const s = new Benchmark.Suite()
+  bs.reverse().forEach(b => {
+    b = b.replace(/[ \n]+/g, " ")
+    s.add(b, eval("() => " + b))
+  })
+  s.on('complete', complete)
+  s.run()
+}, [
+  [
+    'F.fromZipper(F.everywhere(inc, F.toZipper(nested)))',
+  ], [
+    'F.fromZipper(F.everywhere(inc, F.toZipper(vs10)))',
+  ], [
+    'F.fromZipper(F.everywhere(inc, F.toZipper(vs100)))',
+  ], [
+    'F.fromZipper(F.everywhere(inc, F.toZipper(vs1000)))',
+  ]
+])
+
+function complete() {
+  const bs = I.seq(this,
+                   R.values,
+                   R.filter(R.is(Benchmark)),
+                   R.sortBy(R.prop("hz")),
+                   R.reverse)
+  const fastest = I.seq(bs,
+                        R.map(R.prop("hz")),
+                        R.reduce(R.max, 0))
+  bs.forEach(b => {
+    console.log(sprintf('%12s/s %7.2fx  %s',
+                        Math.round(b.hz).toLocaleString(),
+                        fastest/b.hz, b.name))
+  })
+  console.log()
+}
