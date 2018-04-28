63dae09d82949aba8d2edb46ea092e973a76b290
Vesa Karvonen <vesa.a.j.k@gmail.com>
2018-04-28 19:15:23 +0300

    Updated

--- rollup.config.js
+++ rollup.config.js
@@ -1,19 +1,38 @@
-import babel       from "rollup-plugin-babel"
-import nodeResolve from "rollup-plugin-node-resolve"
-import replace     from "rollup-plugin-replace"
-import uglify      from "rollup-plugin-uglify"
+import babel from 'rollup-plugin-babel'
+import nodeResolve from 'rollup-plugin-node-resolve'
+import replace from 'rollup-plugin-replace'
+import uglify from 'rollup-plugin-uglify'
 
-export default {
-  external: ["infestines"],
+const globals = { infestines: 'I' }
+
+const build = ({ NODE_ENV, format, suffix }) => ({
+  external: Object.keys(globals),
+  input: 'src/fastener.js',
   output: {
-    globals: { infestines: 'I' }
+    globals,
+    name: 'F',
+    format,
+    file: `dist/fastener.${suffix}`
   },
   plugins: [
-    process.env.NODE_ENV &&
-      replace({"process.env.NODE_ENV": JSON.stringify(process.env.NODE_ENV)}),
-    nodeResolve({modulesOnly: true}),
+    NODE_ENV && replace({ 'process.env.NODE_ENV': JSON.stringify(NODE_ENV) }),
+    nodeResolve({ modulesOnly: true }),
     babel(),
-    process.env.NODE_ENV === "production" &&
-      uglify()
+    NODE_ENV === 'production' &&
+      uglify({
+        compress: {
+          hoist_funs: true,
+          passes: 3,
+          pure_getters: true,
+          pure_funcs: ['require']
+        }
+      })
   ].filter(x => x)
-}
+})
+
+export default [
+  build({ format: 'cjs', suffix: 'cjs.js' }),
+  build({ format: 'es', suffix: 'es.js' }),
+  build({ format: 'umd', suffix: 'js', NODE_ENV: 'dev' }),
+  build({ format: 'umd', suffix: 'min.js', NODE_ENV: 'production' })
+]
